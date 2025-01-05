6cd1f2b7da0cebb8ebc7668be634ddae5e05781a
Azamat Bayramov <bayramov.azamat04@gmail.com>
2025-01-05 12:48:49 +0300

    Improve config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,9 @@
-from pydantic_settings import SettingsConfigDict, BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     MONGODB_URL: str = "mongodb://mongodb:27017"
     APP_TITLE: str = "ToDo Application"
-
-    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
